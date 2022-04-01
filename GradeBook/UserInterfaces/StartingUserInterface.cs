a5fa9f6bc192c2813f0458b3531f4b6ad390b424
Marcin Dukat <marcindukat2020@gmail.com>
2022-04-01 23:12:56 +0200

    dokończenie isweighted

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -35,21 +35,23 @@ namespace GradeBook.UserInterfaces
         {
             var parts = command.Split(' ');
             
-            if (parts.Length != 3)
+            
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
+            var isWeighted = Boolean.Parse(parts[3]); ;
             BaseGradeBook gradeBook;
             
             if (parts[2].Equals("standard"))
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name,isWeighted);
             }
             else if (parts[2].Equals("ranked"))
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name,isWeighted);
             }
             else
             {
