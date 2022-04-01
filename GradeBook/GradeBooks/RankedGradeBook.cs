c4f49d043f58b55959bad76465ef395b62effce2
Marcin Dukat <marcindukat2020@gmail.com>
2022-04-01 21:32:07 +0200

    Dodanie RankedGradebook i ustawienie Type

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        
+    }
+}
